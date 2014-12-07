a19a67f5d37a6cf212d49a537b0bd7c49870ad9b
todesking <discommunicative@gmail.com>
2014-12-07 17:09:03 +0900

    remove debugprint

--- src/test/scala/monolay.scala
+++ src/test/scala/monolay.scala
@@ -229,7 +229,6 @@ class LayoutSpec extends FunSpec with Matchers {
       subject.withIndent(2) {
         subject.appendText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
       }
-      println(subject)
 
       subject.toString() shouldEqual """
        |# The standard Lorem Ipsum passage, used since the 1500s #
