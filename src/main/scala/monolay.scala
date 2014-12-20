07d855c27ef044d77a60781e812d57639f604f1c
todesking <discommunicative@gmail.com>
2014-12-20 20:09:34 +0900

    refactor

--- src/main/scala/monolay.scala
+++ src/main/scala/monolay.scala
@@ -149,30 +149,46 @@ class Layout(optimalWidth:Int, private var indentLevel:Int = 0) {
     val builder = Table.builder()
     f(builder)
     terminateLine()
-    builder.render(optimalWidth) {line =>
+    builder.render(optimalWidth, DisplayWidthRule.default) {line =>
       appendRaw(line)
       newLine()
     }
   }
 }
 
+trait DisplayWidthRule {
+  def widthOf(s: String): Int
+}
+
+object DisplayWidthRule {
+  val default = new DisplayWidthRule {
+    override def widthOf(s: String): Int =
+      s.split("\n").map { line => displayWidth0(line) }.max
+
+    private[this] def displayWidth0(s: String):Int = {
+      val halfs = """[\u0000-\u007f]""".r.findAllIn(s).size
+      val fulls = s.size - halfs
+      halfs + fulls * 2
+    }
+  }
+}
+
 object Table {
   def builder() = new Builder()
+  case class Row(cols:Seq[Column]) {
+    def size:Int = cols.size
+    def apply(i:Int):Column = cols(i)
+  }
+  object Row {
+    def fromPlain(cols:Seq[String]):Row = {
+      Row(
+        cols.zipWithIndex.map { case (c, i) => Column(i, c) }
+      )
+    }
+    val empty: Row = Row(Seq())
+  }
+  case class Column(num:Int, content:String)
   class Builder {
-    case class Column(num:Int, content:String)
-    case class Row(cols:Seq[Column]) {
-      def size:Int = cols.size
-      def apply(i:Int):Column = cols(i)
-    }
-    object Row {
-      def fromPlain(cols:Seq[String]):Row = {
-        Row(
-          cols.zipWithIndex.map { case (c, i) => Column(i, c) }
-        )
-      }
-      val empty: Row = Row(Seq())
-    }
-    var calcDisplayWidth:(String => Int) = { _.size }
     private var header:Option[Row] = None
     private var rows:Seq[Row] = Seq()
     private var widths: Option[Seq[Int]] = None
@@ -193,22 +209,82 @@ object Table {
       this
     }
 
-    def calcWidths(maxWidth:Int):Seq[Int] = {
-      val raw = (header orElse rows.headOption getOrElse Row.empty).cols.map { case Column(i, name) =>
-          Math.max(calcDisplayWidth(name), rows.map{ r => calcDisplayWidth(r(i).content) }.++(Seq(0)).max)
+    def render(maxWidth: Int, displayWidth: DisplayWidthRule)(println: String=>Unit): Unit = {
+      if(header.isEmpty && rows.isEmpty)
+        return
+
+      var finalWidths = widths getOrElse ColumnWidthRule.optimized(maxWidth, displayWidth).widths(header, rows)
+
+      val println_ = println
+      val renderer = new Renderer {
+        override def println(line: String): Unit = println_(line)
+        override val widths = finalWidths
+        override val displayWidthRule = displayWidth
       }
-      optimizeWidth(raw, maxWidth - (2 * columnSize)/*padding*/ - (columnSize - 1)/*sep*/ - 4/*start/end*/)
+
+      renderer.render(header, rows, displayWidth)
+    }
+  }
+
+  trait ColumnWidthRule {
+    def widths(header: Option[Row], rows: Seq[Row]): Seq[Int]
+  }
+
+  object ColumnWidthRule {
+    def optimized(maxWidth: Int, displayWidthRule: DisplayWidthRule) = new ColumnWidthRule {
+      override def widths(header: Option[Row], rows: Seq[Row]): Seq[Int] = {
+        val raw = (header orElse rows.headOption getOrElse Row.empty).cols.map { case Column(i, name) =>
+            Math.max(displayWidthRule.widthOf(name), rows.map{ r => displayWidthRule.widthOf(r(i).content) }.++(Seq(0)).max)
+        }
+        val columnSize = raw.size
+        optimizeWidth(raw, maxWidth - (2 * columnSize)/*padding*/ - (columnSize - 1)/*sep*/ - 4/*start/end*/)
+      }
+
+      def optimizeWidth(ws:Seq[Int], max:Int) = {
+        if(ws.sum <= max) ws
+        else {
+          val base = max / ws.size
+          val ws2 = ws.map {w => (w, Math.min(w, base))}
+          val rest = max - ws2.map{case (p, r) => r}.sum
+          val div = Math.max(ws2.map{case (p,r) => p - r}.sum, 1)
+          ws2.map { case (prefer, real) => real + rest * (prefer - real) / div}
+        }
+      }
+    }
+  }
+
+  trait Renderer {
+    def println(line: String): Unit
+    val widths: Seq[Int]
+    val displayWidthRule: DisplayWidthRule
+
+    def render(header: Option[Row], rows: Seq[Row], displayWidth: DisplayWidthRule): Unit = {
+      renderBorder('-')
+      header foreach {h =>
+        renderRow(h)
+        renderBorder('=')
+      }
+      rows.foreach(renderRow(_))
+      renderBorder('-')
     }
 
-    def optimizeWidth(ws:Seq[Int], max:Int) = {
-      if(ws.sum <= max) ws
-      else {
-        val base = max / ws.size
-        val ws2 = ws.map {w => (w, Math.min(w, base))}
-        val rest = max - ws2.map{case (p, r) => r}.sum
-        val div = Math.max(ws2.map{case (p,r) => p - r}.sum, 1)
-        ws2.map { case (prefer, real) => real + rest * (prefer - real) / div}
+    def renderBorder(ch: Char) =
+      println("+" + widths.map{w => ch.toString * (w + 2)}.mkString("+") + "+")
+
+    def renderRow(row:Row) = {
+      val subRows:Seq[Seq[String]] = quadrilateralize(row.cols.map(_.content.split("\n").toSeq), "").transpose
+      val maxHeight = subRows.map(_.size).max
+      subRows.zipWithIndex.foreach {case (row, i) =>
+        val sep = if(i == 0) "|" else ":"
+        println(s"$sep " + row.zipWithIndex.map{case (r, i) => pad(r, displayWidthRule.widthOf(_), widths(i), ' ', "...")}.mkString(s" $sep ") + " |")
       }
+    }
+
+    def pad(self:String, widthFunc:(String=>Int), maxWidth:Int, padding:Char, omit:String):String = {
+      val w = widthFunc(self)
+      if(w == maxWidth) self
+      else if(w < maxWidth) self + padding.toString * (maxWidth - w)
+      else shorten(self, widthFunc, maxWidth - widthFunc(omit)) + omit
     }
 
     def quadrilateralize[A](self:Seq[Seq[A]], default:A):Seq[Seq[A]] = {
@@ -218,13 +294,6 @@ object Table {
           if(c < row.size) row(c) else default
         }
       }
-    }
-
-    def pad(self:String, widthFunc:(String=>Int), maxWidth:Int, padding:Char, omit:String):String = {
-      val w = widthFunc(self)
-      if(w == maxWidth) self
-      else if(w < maxWidth) self + padding.toString * (maxWidth - w)
-      else shorten(self, widthFunc, maxWidth - widthFunc(omit)) + omit
     }
 
     def shorten(self:String, widthFunc:(String=>Int), width:Int):String = {
@@ -239,26 +308,6 @@ object Table {
       else shorten0(self, widthFunc, index + 1, widthLeft - w)
     }
 
-    def render(maxWidth:Int)(println:String=>Unit):Unit = {
-      if(header.isEmpty && rows.isEmpty)
-        return
-      var finalWidths = widths getOrElse calcWidths(maxWidth)
-      def rowsep(hbar: String) = println("+" + finalWidths.map{w => hbar * (w + 2)}.mkString("+") + "+")
-      def outRow(row:Row) = {
-        val subRows:Seq[Seq[String]] = quadrilateralize(row.cols.map(_.content.split("\n").toSeq), "").transpose
-        val maxHeight = subRows.map(_.size).max
-        subRows.zipWithIndex.foreach {case (row, i) =>
-          val sep = if(i == 0) "|" else ":"
-          println(s"$sep " + row.zipWithIndex.map{case (r, i) => pad(r, calcDisplayWidth(_), finalWidths(i), ' ', "...")}.mkString(s" $sep ") + " |")
-        }
-      }
-      rowsep("-")
-      header foreach {h =>
-        outRow(h)
-        rowsep("=")
-      }
-      rows.foreach(outRow(_))
-      rowsep("-")
-    }
+
   }
 }
